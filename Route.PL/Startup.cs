4edc49890ad52c44b773b45a489899e3bec3bf32
engmhisham <eng.mhisham96@gmail.com>
2024-08-31 06:28:24 +0300

    DbContext Dependency Injection

--- Route.PL/Startup.cs
+++ Route.PL/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Route.DAL.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,10 +22,15 @@ namespace Route.PL
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
+		// This method gets called by the runtime. Use this method to add services to the DI container.
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+
+			services.AddDbContext<ApplicationDbContext>(options =>
+			{
+				options.UseSqlServer("Server = .; Database = MVCApplication; Trusted_Connection = True");
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
